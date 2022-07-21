c07da2401d27585d7abc083eeed7b0ea5d173f39
Yegor Maystrenko <75858724+Yegor11@users.noreply.github.com>
2022-07-22 02:37:41 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml;
+
+namespace TestTask
+{
+    internal class Program
+    {
+        static int numbersum(string numbers) // splitting number into digits for comparing string elements
+        {
+            int number = Convert.ToInt32(numbers);
+            int result = 0;
+            while (number != 0)
+            {
+                result += number % 10;
+                number /= 10;
+            }
+            return result;
+        }
+        static bool check(string one , string two) // predicat to compare elements in sort algorithm
+        {
+             bool result = true;
+
+             if (numbersum(one) < numbersum(two))
+             {
+                 result = true;
+             }
+             if( numbersum(one) > numbersum(two) )
+             {
+                 result = false;
+             }
+             if (numbersum(one) == numbersum(two))
+             {
+                 char[] charofone = one.ToCharArray();
+                 char[] charoftwo = two.ToCharArray();
+                 int max = Math.Max(one.Length, two.Length);
+                 for (int i = 0; i < max; i++)
+                 {
+                     if (charofone[i] == charoftwo[i])
+                     {
+                         continue;
+                     }
+                     if (charofone[i] < charoftwo[i])
+                     {
+                         result = true;
+                         break;
+                     }
+                     if (charofone[i] > charoftwo[i])
+                     {
+                         result = false;
+                         break;
+                     }
+                 }
+                 
+             }
+             return result;
+         }
+        
+        static List<string> Quicksort(List<string> list) // quicksort algorithm for list
+        {
+            if (list.Count <= 1)
+            {
+                return list;
+            }
+            
+            int pivotPosition = list.Count / 2;
+            string pivotValue = list[pivotPosition];
+            list.RemoveAt(pivotPosition);
+            List<string> smaller = new List<string>();
+            List<string> greater = new List<string>();
+            foreach (var item in list)
+            {
+                if ( check(item,pivotValue))
+                {
+                    smaller.Add(item);
+                }
+                else
+                {
+                    greater.Add(item);
+                }
+            }
+            List<string> sorted = Quicksort(smaller);
+            sorted.Add(pivotValue);
+            sorted.AddRange(Quicksort(greater));
+            return sorted;
+        }
+        
+        static string Order(string input)
+        {
+            // deleting extra spaces from string
+            char[] inputofchars = input.Trim().ToCharArray();
+            string nospacesstring = String.Empty;
+            for (int i = 0; i < inputofchars.Length; i++)
+            {
+                if (! ((inputofchars[i] == ' ') && (inputofchars[ i + 1 ] == ' ')))
+                {
+                    nospacesstring += inputofchars[i];
+                }
+            }
+
+            // creating list to work more comfortable 
+            List<string> numbers = new List<string>();
+            char[] nospacesofchars = nospacesstring.ToCharArray();
+            string number = String.Empty;
+            for (int j = 0; j < nospacesofchars.Length; j++  )
+            {
+                if (nospacesofchars[j] != ' ')
+                {
+                    number += nospacesofchars[j];
+                }
+                else
+                {
+                    numbers.Add(number);
+                    number = String.Empty;
+                }
+            }
+            numbers.Add(number);
+
+            // casting back to string, because of task requirements
+            List<string> sortedlist  = Quicksort(numbers);
+            string sortedstring = String.Empty;
+            foreach (var element in sortedlist)
+            {
+                sortedstring += element;
+                sortedstring += " ";
+            }
+            return sortedstring;
+        }
+
+        public static void Main(string[] args)
+        {
+            
+           string list = "    45 34 24 108 94  76 58 49  64 130 80 49 49 43 900 71  555541 555514 993111 993  ";
+           if (list == String.Empty)
+           {
+               Console.WriteLine("Empty list!");
+           }
+           else
+           {
+               Console.WriteLine(" Was before sorting: " + list );
+               Console.WriteLine(" After sorting: " + Order(list) );
+           }
+        }
+    }
+}
